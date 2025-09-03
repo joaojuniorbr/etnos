be61a2febffc024bac025f3db1fcc1545156a689
João Junior <negrelis@gmail.com>
2025-09-03 08:43:41 -0300

    :fire: remove os rewrites do projeto web por enquanto

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -7,18 +7,18 @@ const nextConfig: NextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
-	async rewrites() {
-		return [
-			{
-				source: '/admin/:path*',
-				destination: `${process.env.ADMIN_URL}/admin/:path*`,
-			},
-			{
-				source: '/estudante/:path*',
-				destination: `${process.env.STUDENT_URL}/estudante/:path*`,
-			},
-		];
-	},
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: '/admin/:path*',
+	// 			destination: `${process.env.ADMIN_URL}/admin/:path*`,
+	// 		},
+	// 		{
+	// 			source: '/estudante/:path*',
+	// 			destination: `${process.env.STUDENT_URL}/estudante/:path*`,
+	// 		},
+	// 	];
+	// },
 };
 
 export default nextConfig;

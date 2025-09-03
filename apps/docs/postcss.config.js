2f197df4cd42bee3737509e31f93266660dbd2cc
João Junior <negrelis@gmail.com>
2025-09-02 23:29:51 -0300

    :tada: init project

---
+++ apps/docs/postcss.config.js
@@ -0,0 +1,3 @@
+import { postcssConfig } from '@etnos/tailwind-config/postcss';
+
+export default postcssConfig;
--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from '@etnos/eslint-config/react-internal';
 
 /** @type {import("eslint").Linter.Config} */
 export default config;

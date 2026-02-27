5e4206521badfcb0c78ef16ae905584e1b234045
Joao Junior <negrelis@gmail.com>
2026-02-27 15:39:33 -0300

    feat(home): [CU-86afq1pxn] Adicionar componente de Pronto para começar a jornada

---
+++ apps/web/components/pages/home/Journey/Journey.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@etnos/ui';
+import { Image } from 'antd';
+import Link from 'next/link';
+
+export const Journey = () => (
+	<section className='p-6 md:py-16 lg:py-20'>
+		<div className='max-w-5xl mx-auto text-center'>
+			<h2 className='uppercase text-xl text-primary font-black md:text-4xl mb-2'>
+				Pronto para começar sua jornada?
+			</h2>
+			<p className='text-sm md:text-base mb-8 lg:text-xl lg:leading-7'>
+				Descubra culturas, aprenda com respeito e jogue de forma divertida e
+				educativa. Cadastre-se gratuitamente e mergulhe em experiências que
+				transformam o conhecimento em empatia.
+			</p>
+
+			<Link href='/login'>
+				<Button type='secondary' size='xl'>
+					COMECE AGORA
+				</Button>
+			</Link>
+
+			<div className='bg-white max-w-240 mx-auto shadow-xl mt-10 p-2 rounded md:rounded-2xl md:p-6 lg:p-8'>
+				<figure className='m-0 bg-[#fbfbfb] rounded px-2 md:rounded-2xl md:p-6 lg:p-10'>
+					<Image
+						preview={false}
+						src='/estudante/persona-group.jpg'
+						className='w-full'
+					/>
+				</figure>
+			</div>
+		</div>
+	</section>
+);

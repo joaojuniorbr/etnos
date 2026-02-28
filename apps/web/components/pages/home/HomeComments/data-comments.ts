321ae6e72d00cfc6f025a9c8e456dcd27ab02c75
Joao Junior <negrelis@gmail.com>
2026-02-27 21:52:52 -0300

    feat(home): adicionado o componente de comentários

---
+++ apps/web/components/pages/home/HomeComments/data-comments.ts
@@ -0,0 +1,36 @@
+export const dataComments = [
+	{
+		name: 'Maria Silva',
+		rate: 5,
+		comment:
+			'Meu filho amou! Aprendeu muito sobre diferentes culturas de forma divertida.',
+		image: '/images/avatar/avatar-2.jpg',
+	},
+	{
+		name: 'Carlos Santos',
+		rate: 4,
+		comment:
+			'Ótimo aplicativo educativo. Os jogos são interativos e mantêm as crianças engajadas.',
+		image: '/images/avatar/avatar-1.jpg',
+	},
+	{
+		name: 'Ana Costa',
+		rate: 5,
+		comment:
+			'Recomendo! Excelente ferramenta para ensinar respeito à diversidade desde cedo.',
+		image: '/images/avatar/avatar-3.jpg',
+	},
+	{
+		name: 'Pedro Oliveira',
+		rate: 3,
+		comment:
+			'Bom conteúdo educativo, mas gostaria de mais desafios para crianças maiores.',
+		image: '/images/avatar/avatar-5.jpg',
+	},
+	{
+		name: 'Lucia Ferreira',
+		rate: 5,
+		comment: 'Perfeito para aprender sobre etnias de forma lúdica e segura!',
+		image: '/images/avatar/avatar-4.jpg',
+	},
+];
